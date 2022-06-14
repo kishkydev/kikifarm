ffb5588e79e8e0855ff3f22cf4773b081ba8be3a
Olukayode Fadairo <101965202+fatherherok@users.noreply.github.com>
2022-06-14 17:26:12 +0100

    Update connect.inc.php

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,8 +1,8 @@
 <?php
-	$db_host = 'kikiserver2.mysql.database.azure.com';
-	$db_user = 'kikiadmin';
-	$db_pass = 'Incorrect9@';
-	$db_name = 'kikifarm';
+	$db_host = 'kikifarmdbserver.mysql.database.azure.com';
+	$db_user = 'kiki';
+	$db_pass = '4h5MbZsfrS9ixup';
+	$db_name = 'kikifarm-db';
 	$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die('no connection to the MYSL server');
 if(mysqli_connect_errno()){
 	echo 'Failed to connect to the MYSQL: '.mysqli_connect_error();
